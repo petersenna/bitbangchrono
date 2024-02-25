41288e143d550b6811cc00a492f3d483a31fe6e5
Peter Senna Tschudin <peter.senna@spearline.com>
2024-02-25 17:03:12 +0100

    Refactor to use different functions
    Changes the code from a sigle function to:
     int initialize_ftdi(struct ftdi_context **ftdi, int vendor, int product, bool verbose);
     void set_bitbang_mode(struct ftdi_context *ftdi, bool verbose);
     void write_data(struct ftdi_context *ftdi, unsigned char data, bool verbose);
     void toggle_bits(struct ftdi_context *ftdi, bool verbose);
     void cleanup(struct ftdi_context *ftdi, bool verbose);
    
    Also update the coding style to have the { on the same line as the
    statement.
    
    Signed-off-by: Peter Senna Tschudin <peter.senna@gmail.com>

--- bitbangchrono.c
+++ bitbangchrono.c
@@ -4,82 +4,107 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdbool.h>
 #include <ftdi.h>
 
-int main(int argc, char **argv)
-{
+#define USB_VENDOR_ID 0x0403
+#define USB_PRODUCT_ID 0x6001
+
+// Function declarations
+int initialize_ftdi(struct ftdi_context **ftdi, int vendor, int product, bool verbose);
+void set_bitbang_mode(struct ftdi_context *ftdi, bool verbose);
+void write_data(struct ftdi_context *ftdi, unsigned char data, bool verbose);
+void toggle_bits(struct ftdi_context *ftdi, bool verbose);
+void cleanup(struct ftdi_context *ftdi, bool verbose);
+
+int main(int argc, char **argv) {
     struct ftdi_context *ftdi;
-    int f,i;
-    unsigned char buf[1];
-    int retval = 0;
+    int retval;
 
-    if ((ftdi = ftdi_new()) == 0)
-    {
-        fprintf(stderr, "ftdi_new failed\n");
+    bool verbose = false;
+
+    retval = initialize_ftdi(&ftdi, USB_VENDOR_ID, USB_PRODUCT_ID, verbose);
+    if (retval != 0)
         return EXIT_FAILURE;
+
+    set_bitbang_mode(ftdi, verbose);
+    write_data(ftdi, 0x00, verbose); // Turn everything off
+
+    toggle_bits(ftdi, verbose);
+
+    cleanup(ftdi, verbose);
+
+    return EXIT_SUCCESS;
+}
+
+int initialize_ftdi(struct ftdi_context **ftdi, int vendor, int product, bool verbose) {
+    int f;
+
+    if ((*ftdi = ftdi_new()) == 0) {
+        if (verbose)
+            fprintf(stderr, "ftdi_new failed\n");
+        return 1;
     }
 
-    f = ftdi_usb_open(ftdi, 0x0403, 0x6001);
+    f = ftdi_usb_open(*ftdi, vendor, product);
 
-    if (f < 0 && f != -5)
-    {
-        fprintf(stderr, "unable to open ftdi device: %d (%s)\n", f, ftdi_get_error_string(ftdi));
-        retval = 1;
-        goto done;
+    if (f < 0 && f != -5) {
+        if (verbose)
+            fprintf(stderr, "unable to open ftdi device: %d (%s)\n", f, ftdi_get_error_string(*ftdi));
+        ftdi_free(*ftdi);
+        return 1;
     }
 
-    printf("ftdi open succeeded: %d\n",f);
+    if (verbose)
+        printf("ftdi open succeeded: %d\n", f);
 
-    printf("enabling bitbang mode\n");
+    return 0;
+}
+
+void set_bitbang_mode(struct ftdi_context *ftdi, bool verbose) {
+    if (verbose)
+        printf("enabling bitbang mode\n");
+
     ftdi_set_bitmode(ftdi, 0xFF, BITMODE_BITBANG);
+}
 
-    sleep(3);
+void write_data(struct ftdi_context *ftdi, unsigned char data, bool verbose) {
+    int f = ftdi_write_data(ftdi, &data, 1);
 
-    buf[0] = 0xFF;
-    printf("turning everything on\n");
-    f = ftdi_write_data(ftdi, buf, 1);
-    if (f < 0)
-    {
-        fprintf(stderr,"write failed for 0x%x, error %d (%s)\n",buf[0],f, ftdi_get_error_string(ftdi));
+    if (f < 0) {
+        fprintf(stderr, "write failed for 0x%x, error %d (%s)\n", data, f, ftdi_get_error_string(ftdi));
+    } else {
+        if (verbose)
+            printf("Data 0x%02x written successfully\n", data);
     }
+}
 
-    sleep(3);
+void toggle_bits(struct ftdi_context *ftdi, bool verbose) {
+    unsigned char buf;
+    for (int i = 0; i < 32; i++) {
+        buf = 1 << (i % 8);
 
-    buf[0] = 0x00;
-    printf("turning everything off\n");
-    f = ftdi_write_data(ftdi, buf, 1);
-    if (f < 0)
-    {
-        fprintf(stderr,"write failed for 0x%x, error %d (%s)\n",buf[0],f, ftdi_get_error_string(ftdi));
-    }
+        if (verbose) {
+            if (i > 0 && (i % 8) == 0)
+                printf("\n");
 
-    sleep(3);
-
-    for (i = 0; i < 32; i++)
-    {
-        buf[0] = 1 << (i % 8);
-        if ( i > 0 && (i % 8) == 0)
-        {
-            printf("\n");
+            printf("%02hhx ", buf);
         }
-        printf("%02hhx ",buf[0]);
+
         fflush(stdout);
-        f = ftdi_write_data(ftdi, buf, 1);
-        if (f < 0)
-        {
-            fprintf(stderr,"write failed for 0x%x, error %d (%s)\n",buf[0],f, ftdi_get_error_string(ftdi));
-        }
+        write_data(ftdi, buf, verbose);
         sleep(1);
     }
 
-    printf("\n");
+    if (verbose)
+        printf("\n");
+}
 
-    printf("disabling bitbang mode\n");
+void cleanup(struct ftdi_context *ftdi, bool verbose) {
+    if (verbose)
+        printf("disabling bitbang mode\n");
+
     ftdi_disable_bitbang(ftdi);
-
     ftdi_usb_close(ftdi);
-done:
     ftdi_free(ftdi);
-
-    return retval;
 }
